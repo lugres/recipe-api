a6c882a7c024874da49fa4977bc13466b5767569
Lugres <lugres@gmail.com>
2024-09-18 21:48:27 -0400

    Built recipe API using TDD

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,8 +2,12 @@
 Tests for models
 """
 
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -13,7 +17,10 @@ class ModelTests(TestCase):
         """Test creating a user with an email is successful."""
         email = "test@example.com"
         password = "testpass123"
-        user = get_user_model().objects.create_user(email=email, password=password)
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -46,3 +53,19 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "testpsw123",
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title="Sample recipe name",
+            time_minutes=5,
+            price=Decimal("5.50"),
+            description="Sample recipe description",
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
---
+++ app/recipe/urls.py
@@ -0,0 +1,21 @@
+"""
+URL mappings for the recipe app.
+"""
+
+from django.urls import (
+    path,
+    include,
+)
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+app_name = "recipe"
+
+router = DefaultRouter()
+router.register("recipes", views.RecipeViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
